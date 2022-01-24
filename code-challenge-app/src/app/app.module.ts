375fa4a4b7d1bd2c88356fd9df6a72512a07e5cf
slauney <slauney@octolabs.io>
2022-01-24 10:27:05 -0600

    updated to lastest

--- code-challenge-app/src/app/app.module.ts
+++ code-challenge-app/src/app/app.module.ts
@@ -1,40 +1,38 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { MatTableModule } from '@angular/material/table';
-import { MatChipsModule } from '@angular/material/chips';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { ChartComponent } from './chart/chart.component';
-import { GraphComponent } from './visuals/graph/graph.component';
-import { NodeVisualComponent } from './visuals/shared/node-visual/node-visual.component';
-import { LinkVisualComponent } from './visuals/shared/link-visual/link-visual.component';
-import { ZoomableDirective } from './d3/directives/zoomable.directive';
-import { DraggableDirective } from './d3/directives/draggable.directive';
 import { D3Service } from './d3/d3.service';
 import { ClickableDirective } from './d3/directives/clickable.directive';
+import { DraggableDirective } from './d3/directives/draggable.directive';
+import { ZoomableDirective } from './d3/directives/zoomable.directive';
+import { UserDetailsDialogComponent } from './user-details-dialog/user-details-dialog.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { UserTableComponent } from './user-table/user-table.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSliderModule } from '@angular/material/slider';
-import { UserDetailsDialogComponent } from './user-details-dialog/user-details-dialog.component';
+import { UserComponent } from './user/user.component';
+import { GraphComponent } from './visuals/graph/graph.component';
+import { LinkVisualComponent } from './visuals/shared/link-visual/link-visual.component';
+import { NodeVisualComponent } from './visuals/shared/node-visual/node-visual.component';
 
 @NgModule({
   declarations: [
@@ -60,7 +58,6 @@ import { UserDetailsDialogComponent } from './user-details-dialog/user-details-d
     MatCardModule,
     MatListModule,
     MatExpansionModule,
-    FlexLayoutModule,
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
