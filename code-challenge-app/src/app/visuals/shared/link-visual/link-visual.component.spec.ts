8dcac87a0070ce9e9fbc653a08fff93f48498aa4
slauney <slauney@octolabs.io>
2022-01-24 11:32:07 -0600

    12 - 3

--- code-challenge-app/src/app/visuals/shared/link-visual/link-visual.component.spec.ts
+++ code-challenge-app/src/app/visuals/shared/link-visual/link-visual.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LinkVisualComponent } from './link-visual.component';
 
@@ -6,7 +6,7 @@ describe('LinkVisualComponent', () => {
   let component: LinkVisualComponent;
   let fixture: ComponentFixture<LinkVisualComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LinkVisualComponent ]
     })
--- code-challenge-app/src/app/visuals/shared/node-visual/node-visual.component.spec.ts
+++ code-challenge-app/src/app/visuals/shared/node-visual/node-visual.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NodeVisualComponent } from './node-visual.component';
 
@@ -6,7 +6,7 @@ describe('NodeVisualComponent', () => {
   let component: NodeVisualComponent;
   let fixture: ComponentFixture<NodeVisualComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NodeVisualComponent ]
     })
